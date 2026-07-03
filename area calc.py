73a3433075c5cb76f3f83335317315ca0cc40c75
Kingfavour01 <kingfresh1000upgrades@gmail.com>
2026-07-03 22:35:40 +0100

    Initial commit

---
+++ area calc.py
@@ -0,0 +1,5 @@
+length = input("Enter the length of the rectangle: ")
+width = input("Enter the width of the rectangle: ")
+
+area = float(length) * float(width)
+print(f"The area of the rectangle is: {area}") 
---
+++ sentece gamr.py
@@ -0,0 +1,9 @@
+noun = input("Enter a noun: ")
+adjective = input("Enter an adjective: ")
+adjective_2 = input("Enter another adjective: ")
+
+
+
+print(f"Today I went to the zoo. I saw a(n) {adjective} {noun} jumping up and down in its tree. It {adjective_2} through the large tunnel that led to its {adjective} {noun}.")
+print(f"I got some peanuts and passed them through the cage to a gigantic gray {noun} towering above my head. Feeding that animal made me hungry. I went to get a {adjective} scoop of ice cream. It filled my stomach. Afterwards I had to {adjective_2} to catch our bus. When I got home I {adjective} my mom for a {adjective} day at the zoo.")
+print(f" I really loved the opportunity to see a(n) {adjective} {noun} up close.")
